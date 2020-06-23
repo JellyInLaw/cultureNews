e8a85cce3a555805d104289ac281841052532480
Maxim <waiyyshik@gmail.com>
2020-06-23 19:20:53 +0500

    Made function in js-file.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ const burger = document.querySelector(".header__burger");
 const headerNav = document.querySelector(".header__nav");
 const header = document.querySelector(".header");
 
-burger.addEventListener("click", function () {
+let showBurgerMenu = () => {
   burger.classList.toggle("active");
   header.classList.toggle("burger-active");
-});
-
-burger.addEventListener("click", function () {
   headerNav.classList.toggle("active");
-});
+};
+
+burger.addEventListener("click", showBurgerMenu);
